829ebf6cdec302b4e80f9f6bf2cf66033354b874
IbrahimBaniSalim <ibrahim.banisalim@gmail.com>
2021-06-15 18:38:17 +0300

    edit

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ require('dotenv').config();
 const app = express();
 const db = require('./db');
 
-const users = require('./book');
+const book = require('./book');
 
 
 app.get('/book', book.findAllbook);
-app.get('/bookorder', book.orderbook);
-app.get('/pricenull', book.pricenNll);
-app.get('/priceNOTnull', book.pricenNotNll);
+app.get('/bookOrder', book.orderbook);
+app.get('/priceNull', book.pricenNll);
+app.get('/priceNOTNull', book.pricenNotNll);
 app.post('/book', book.addbook);
 app.put('/books:book_id', book.editbook)
-app.delet('/books:book_id', book.deletbook)
+app.delete('/books:book_id', book.deletbook)
 const PORT = 3000;
 
 app.listen(PORT, () => {
